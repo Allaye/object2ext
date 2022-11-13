471e3277b0b15020e44d6d602ba4ba3d5c9facbe
Kolade Gideon <kolade199@gmail.com>
2022-11-13 21:53:54 +0000

    continue work on the xlx feature

--- xlx.js
+++ xlx.js
@@ -1,25 +1,76 @@
 
 
 /**
- * Converts an array of objects into a Excel file.
+ * Converts an array of objects into an Excel file.
  */
 class Object2Xlsx {
-
-
-    /**
-     * @description - the Object2Xlsx constructor expects an array of object anything other than that will result in an error
-     * @param {Array<object>} arrayOfObjects - 
-     */
-    constructor(arrayOfObjects) {
-        if (!Array.isArray(arrayOfObjects) || arrayOfObjects.length === 0) {
-            throw new Error("Object2Xlsx expects an array of objects, that is not empty");
-        }
-        
-        this.arrayOfObjects = arrayOfObjects;
-
+  /**
+   * @description - the Object2Xlsx constructor expects an array of object anything other than that will result in an error
+   * @param {Array<object>} arrayOfObjects -
+   */
+  constructor(arrayOfObjects) {
+    if (!isArrayOK(arrayOfObjects)) {
+      throw new Error(
+        "Object2Xlsx expects an array of objects, that is not empty"
+      );
     }
-        
 
+    this.arrayOfObjects = arrayOfObjects;
+  }
+
+  //TODO - implement the method the calls the other methods to create the XML/Excel content
+  //TODO - implement the method that saves the XML/Excel content to a file
+  //TODO - implement the method that returns the XML/Excel content as a string
+  //TODO - implement the method that generates the XML header
+  //TODO - implement the method that generates the XML start workbook
+  //TODO - implement the method that generates the XML styles
+  //TODO - implement the method that generates the XML start worksheet
+  //TODO - implement the method that generates the XML end worksheet
+  //TODO - implement the method that generates the XML end workbook
+  //TODO - implement the method that generates the XML footer
+  //TODO - implement the method that generates the XML start row
+  //TODO - implement the method that generates the XML end row
+
+  generateXML() {}
+
+  /**
+   * @description - this method returns the XML header
+   * @param {*} string
+   * @returns
+   */
+  generateHeaders(string) {
+    string += '<?xml version="1.0" encoding="UTF-8"?>';
+    string += '<?mso-application progid="Excel.Sheet"?>';
+    return string;
+  }
+
+  /**
+   * @description - this method returns the XML start workbook
+   * @param {*} string 
+   * @returns 
+   */
+  generateStartWorkbook(string){
+    string +=
+      '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">';
+    return string;
+  }
+}
+
+function isEmptyObject(obj) {
+  // console.log(Object.keys(obj).length)
+  return Object.keys(obj).length === 0;
+}
+
+// LOOP THROUGH AN ARRAY OF OBJECTS
+function isArrayOK(arr) {
+    if (arr.length === 0) return false;
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(arr[i]);
+        if (isEmptyObject(arr[i]) || arr[i].constructor.name != "Object") {
+        return false;
+        }
+    }
+    return true;
 }
 
 module.exports = Object2Xlsx;
